21cf9653a2dd8a49b3a543637499e45c7706ad0d
dfaparicio13 <dfapariciocastro13@gmail.com>
2025-11-12 17:03:55 -0500

    Mejoras trabajadas con Quasar 9

--- Carrito/vite.config.js
+++ Carrito/vite.config.js
@@ -1,6 +1,6 @@
-import { fileURLToPath } from 'node:url'
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 // https://vite.dev/config/
@@ -10,17 +10,25 @@ export default defineConfig({
       template: { transformAssetUrls },
     }),
 
-    // @quasar/plugin-vite options list:
-    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
       sassVariables: fileURLToPath(
-        new URL("./src/quasar-variables.sass", import.meta.url)
-      )
-    })
+        new URL('./src/quasar-variables.sass', import.meta.url)
+      ),
+    }),
   ],
-  base: './', 
+
+  // 👇 muy importante para que las rutas de imágenes funcionen tras el build
+  base: './',
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)), // acceso directo a /src
+    },
+  },
+
   build: {
     outDir: 'dist',
     emptyOutDir: true,
+    assetsDir: 'assets', // asegura que las imágenes estén en /assets
   },
-});
+})
